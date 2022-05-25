8d96c48340f755b08479d49a442bd23a15dcf034
Lisa Darlene Bey <104466267+LDBey@users.noreply.github.com>
2022-05-24 22:17:39 -0400

    nupaths-trippi-may-2022

---
+++ nupaths-trippi-may-2022/README.md
@@ -0,0 +1,19 @@
+# TRIPPI Template
+
+
+## Running the fontend
+- open another terminal in vscode
+- ensure you are in the project root directory
+- change directory into the `fontend` directory, like so:
+```bash
+cd frontend
+```
+- if this is the first time running the app, install dependencies, like so:
+```bash
+npm install
+```
+- to run the app, simply: 
+```bash
+npm start
+```
+- to stop the app, in vscode's terminal hit: `ctrl` + `c`
---
+++ nupaths-trippi-may-2022/frontend/js/locations.js
@@ -0,0 +1,70 @@
+const ATTRIBUTES = [
+  'image',
+  'name',
+  'description',
+  'city',
+  'state',
+  'zipCode',
+  'country',
+  'latitude',
+  'longitude',
+  'likes'
+];
+
+const getLocations = async () => {
+  try {
+    // fetch data
+    // const res = await fetch(window.trippi?.api + 'landmark', {
+    //   credentials: 'include',
+    //   headers: {
+    //     ...window.trippi?.getHeaders()
+    //   }
+    // });
+    // const data = await res.json();
+    //
+    // if (!res.ok && (res.status === 401 || res.status === 400)) {
+    //   window.openNotification('Token expired.');
+    //   window.signOut();
+    // }
+
+    const locationWrapper = document.getElementById('locations-wrapper');
+
+    const data =  [
+      {
+        "Location": {
+          "city": "London",
+          "comments": ["Big Ben", "Warm beer"],
+          "country": "UK",
+          "description": "Nice",
+          "image": "",
+          "latitude": "",
+          "likes": 12345,
+          "longitude": "",
+          "name": "London",
+          "state": "",
+          "streetAddress": "",
+          "zipCode": ""
+        }
+      }
+      ]
+
+    data?.forEach((location) => {
+      const locationelement = document.createElement('landmark-component');
+
+      ATTRIBUTES.forEach(attr => {
+        locationelement.setAttribute(attr, location[attr])
+      });
+      locationWrapper.appendChild(locationelement);
+    });
+  } catch (error) {
+    window.openNotification(error?.message || error?.error || error);
+    console.error(error?.message || error?.error || error);
+  }
+}
+
+
+const onLoad = () => {
+  getLocations();
+}
+
+window.addEventListener('app-loaded', onLoad);
